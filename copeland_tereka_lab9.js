dd8a5b1fd242b56d71bf9e1efda8189e648bb7a6
terekacopeland <starsxmexico@live.com>
2014-03-22 14:14:03 -0400

    commit 2

--- copeland_tereka_lab9.js
+++ copeland_tereka_lab9.js
@@ -1,1 +1,20 @@
-alert("JavaScript works!");
+//alert("JavaScript works!");
+//SDI 1403
+//Lab 9
+//Number and Date Objects
+//Tereka Copeland
+
+//problem 1
+var formatNumber = function() {
+	
+	var forPI = (Math.PI);
+	console.log(Math.PI);
+	console.log("PI is equal to " + Math.PI);
+	
+	
+	console.log(forPI.toFixed(5));
+	console.log("If you fix PI to the 6th decimal the result is " + forPI.toFixed(6));
+	
+};
+
+formatNumber();
