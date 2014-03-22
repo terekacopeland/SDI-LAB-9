ef3a310a351720340d745c8c73dcfa08816070f6
terekacopeland <starsxmexico@live.com>
2014-03-22 16:08:58 -0400

    fixing problem 2

--- copeland_tereka_lab9.js
+++ copeland_tereka_lab9.js
@@ -6,15 +6,14 @@
 
 //problem 1
 var formatNumber = function() {
-	
+
 	var forPI = (Math.PI);
 	console.log(Math.PI);
 	console.log("PI is equal to " + Math.PI);
-	
-	
+
 	console.log("If you fix PI to the 5th decimal the result is " + forPI.toFixed(5));
 	console.log("If you fix PI to the 6th decimal the result is " + forPI.toFixed(6));
-	
+
 };
 
 //formatNumber();
@@ -23,37 +22,41 @@ var formatNumber = function() {
 var numberString = function() {
 	var myString = "25.2354764";
 	console.log("My age is " + myString + ".");
-	                           
-	//var myFloat = parseInt(myString);   
-	
-	if (myString === true) {
+
+	//var myFloat = parseInt(myString);
+
+	if (isNaN(myString)) {
+
+		//console.log("String is a number!");
 		
-    console.log("String is a number!");
-    
-} else {
-			
-    //console.log("String is a decimal! Press enter to convert the age.");
-    
-	var myInt = parseInt(myString);   
-                                   
-console.log("My age is " + parseInt(myString));
-};
+		return "String is not a number";
+
+	} else {
+
+		//console.log("String is a decimal! Press enter to convert the age.");
+
+		var myInt = parseInt(myString);
+		
+		//console.log("My age is " + parseInt(myString));
+		
+		return myInt;
+	};
 
 };
 
-//numberString();
+console.log("My age returned as a number is " + numberString() + ".");
 
 //problem 3
 var myFunction = function() {
-        
-        var stringDate1 = new Date("9/01/1988")
-        var stringDate2 = new Date("10/02/1988")
-        
-		var timeDiff = Math.ceil(stringDate2.getTime() - stringDate1.getTime());
-		var diffDays = Math.ceil(timeDiff / (1000 * 3600 * 24)); 
 
-console.log("The day difference between the two dates is " + diffDays);
-//console.log("The difference between the two dates in hours is " + timeDiff);
+	var stringDate1 = new Date("9/01/1988")
+	var stringDate2 = new Date("10/02/1988")
+
+	var timeDiff = Math.ceil(stringDate2.getTime() - stringDate1.getTime());
+	var diffDays = Math.ceil(timeDiff / (1000 * 3600 * 24));
+
+	console.log("The day difference between the two dates is " + diffDays);
+	//console.log("The difference between the two dates in hours is " + timeDiff);
 };
 
-myFunction();
+//myFunction();
